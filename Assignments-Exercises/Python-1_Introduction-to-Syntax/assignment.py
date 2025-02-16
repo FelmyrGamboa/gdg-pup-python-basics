b2812b48e1cc4aedaae50f9353d19e33fee4d4b2
FelmyrGamboa <felmyrgamboa23@gmail.com>
2025-02-16 23:21:46 +0800

    Finished first python exercise

---
+++ Assignments-Exercises/Python-1_Introduction-to-Syntax/assignment.py
@@ -0,0 +1,20 @@
+name = "Felmyr Jhude"  # string
+age = 20        # integer
+height = 5.6     # float
+is_student = True  # boolean
+hobbies = ["Coding", "Badminton", "Listening to Music", "Watching Series"]
+
+sum_result = age + height
+product_result = age * height
+
+greeting = "Hello, " + name + "!"
+print(greeting)
+
+print("Sum of age and height:", sum_result)
+print("Product of age and height:", product_result)
+
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
+print("Type of hobbies:", type(hobbies))
